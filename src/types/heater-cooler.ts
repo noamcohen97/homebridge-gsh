98822dd3766e96771839a7fe5fcc71a10afe2aee
Noam Cohen <noam@noam.me>
2025-02-17 13:51:40 +0200

    Update heater-cooler.ts

--- src/types/heater-cooler.ts
+++ src/types/heater-cooler.ts
@@ -37,10 +37,10 @@ export class HeaterCooler extends ghToHap implements ghToHap_t {
       queryOnlyOnOff: false,
     };
 
-    const type = 'action.devices.types.THERMOSTAT';
+    let type = 'action.devices.types.THERMOSTAT';
 
     if (service.serviceCharacteristics.find(x => x.uuid === Characteristic.RotationSpeed)) {
-      // type = 'action.devices.types.AC_UNIT';
+      type = 'action.devices.types.AC_UNIT';
       traits.push('action.devices.traits.FanSpeed');
       attributes.supportsFanSpeedPercent = true;
     }
